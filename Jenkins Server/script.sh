2d76453d604fc8a997570145187ac2ada8f60e97
AbhishekS07 <abhisheksawanurkar@gmail.com>
2023-12-22 15:08:27 +0530

    script change

--- Jenkins Server/script.sh
+++ Jenkins Server/script.sh
@@ -30,12 +30,22 @@ sudo systemctl start jenkins
 sudo apt install -y git
 
 # Install Terraform
+sudo apt-get install
+sudo apt install gnupg -y
 sudo apt-get install -y gnupg software-properties-common
 sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
 sudo apt-get update && sudo apt-get install terraform
 
 # Install kubectl
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+#sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+#sudo chmod +x ./kubectl
+#sudo mv ./kubectl /usr/local/bin/kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+kubectl version --short --client
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
