3b763bf938a3ca37ff61d7f586f6c5becd41373e
AbhishekS07 <abhisheksawanurkar@gmail.com>
2023-12-22 12:44:55 +0530

    Adding files to repo

---
+++ Jenkins Server/script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+#Java installaion
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+java -version
+
+#long term support package release
+sudo apt update
+sudo apt install wget -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+#Enable Jenkins
+sudo systemctl enable jenkins
+
+#Start Jenkins
+sudo systemctl start jenkins
+
+# Install Git
+sudo apt install -y git
+
+# Install Terraform
+sudo apt-get install -y gnupg software-properties-common
+sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
+sudo apt-get update && sudo apt-get install terraform
+
+# Install kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
