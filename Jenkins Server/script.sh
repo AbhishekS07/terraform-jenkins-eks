8efa4b6cd603401eea6deb391bdf3fcad01963ca
AbhishekS07 <abhisheksawanurkar@gmail.com>
2023-12-23 11:28:45 +0530

    updated script

--- Jenkins Server/script.sh
+++ Jenkins Server/script.sh
@@ -19,6 +19,7 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
+sudo 
 
 #Enable Jenkins
 sudo systemctl enable jenkins
@@ -26,32 +27,20 @@ sudo systemctl enable jenkins
 #Start Jenkins
 sudo systemctl start jenkins
 
+#install awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+
 # Install Git
 sudo apt install -y git
 
 # Install Terraform
-#!/bin/bash
-
-# Update the package repositories
-sudo apt update -y
-
-# Install unzip if not already installed
-sudo apt install unzip  -y
-
-# Download the latest version of Terraform
-TERRAFORM_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f 4)
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-
-# Unzip the downloaded file and move it to the /usr/local/bin directory
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
-
-# Clean up downloaded files
-rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-
-# Verify the installation
-terraform --version
-
+sudo apt update 
+sudo apt install gnupg -y
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
 
 # Install kubectl
 #sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
@@ -65,3 +54,4 @@ kubectl version --short --client
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+
--- Jenkins Server/variables.tf
+++ Jenkins Server/variables.tf
@@ -7,6 +7,7 @@ variable "public_subnets" {
   description = "Subnets CIDR"
   type        = list(string)
 }
+
 variable "instance_type" {
   description = "Instance Type"
   type        = string
